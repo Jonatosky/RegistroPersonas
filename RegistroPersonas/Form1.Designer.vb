89b424d8f12bceba420d55c9bbcf649970efb4bf
Jonathan <Jonatosky@gmail.com>
2025-10-08 14:36:24 -0300

    roberto

--- RegistroPersonas/Form1.Designer.vb
+++ RegistroPersonas/Form1.Designer.vb
@@ -46,57 +46,63 @@ Partial Class Form1
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(11, 27)
+        Label1.Location = New Point(11, 24)
         Label1.Name = "Label1"
-        Label1.Size = New Size(31, 20)
+        Label1.Size = New Size(30, 18)
         Label1.TabIndex = 0
         Label1.Text = "Rut"
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(14, 75)
+        Label2.Location = New Point(14, 68)
         Label2.Name = "Label2"
-        Label2.Size = New Size(64, 20)
+        Label2.Size = New Size(60, 18)
         Label2.TabIndex = 1
         Label2.Text = "Nombre"
         ' 
         ' Label3
         ' 
         Label3.AutoSize = True
-        Label3.Location = New Point(14, 119)
+        Label3.Location = New Point(14, 107)
         Label3.Name = "Label3"
-        Label3.Size = New Size(72, 20)
+        Label3.Size = New Size(62, 18)
         Label3.TabIndex = 2
         Label3.Text = "Apellidos"
         ' 
         ' txtRut
         ' 
-        txtRut.Location = New Point(99, 27)
+        txtRut.BackColor = SystemColors.InactiveCaption
+        txtRut.ForeColor = SystemColors.MenuHighlight
+        txtRut.Location = New Point(99, 24)
         txtRut.Name = "txtRut"
-        txtRut.Size = New Size(191, 27)
+        txtRut.Size = New Size(191, 26)
         txtRut.TabIndex = 3
         ' 
         ' txtNombre
         ' 
-        txtNombre.Location = New Point(99, 75)
+        txtNombre.BackColor = SystemColors.InactiveCaption
+        txtNombre.ForeColor = SystemColors.MenuHighlight
+        txtNombre.Location = New Point(99, 68)
         txtNombre.Name = "txtNombre"
-        txtNombre.Size = New Size(188, 27)
+        txtNombre.Size = New Size(188, 26)
         txtNombre.TabIndex = 4
         ' 
         ' txtApellido
         ' 
-        txtApellido.Location = New Point(101, 119)
+        txtApellido.BackColor = SystemColors.InactiveCaption
+        txtApellido.ForeColor = SystemColors.HotTrack
+        txtApellido.Location = New Point(101, 107)
         txtApellido.Name = "txtApellido"
-        txtApellido.Size = New Size(186, 27)
+        txtApellido.Size = New Size(186, 26)
         txtApellido.TabIndex = 5
         ' 
         ' rbMasculino
         ' 
         rbMasculino.AutoSize = True
-        rbMasculino.Location = New Point(31, 197)
+        rbMasculino.Location = New Point(9, 179)
         rbMasculino.Name = "rbMasculino"
-        rbMasculino.Size = New Size(97, 24)
+        rbMasculino.Size = New Size(91, 22)
         rbMasculino.TabIndex = 6
         rbMasculino.TabStop = True
         rbMasculino.Text = "Masculino"
@@ -105,9 +111,9 @@ Partial Class Form1
         ' rbFemenino
         ' 
         rbFemenino.AutoSize = True
-        rbFemenino.Location = New Point(183, 199)
+        rbFemenino.Location = New Point(107, 179)
         rbFemenino.Name = "rbFemenino"
-        rbFemenino.Size = New Size(95, 24)
+        rbFemenino.Size = New Size(92, 22)
         rbFemenino.TabIndex = 7
         rbFemenino.TabStop = True
         rbFemenino.Text = "Femenino"
@@ -116,9 +122,9 @@ Partial Class Form1
         ' NoEspecifica
         ' 
         NoEspecifica.AutoSize = True
-        NoEspecifica.Location = New Point(355, 199)
+        NoEspecifica.Location = New Point(197, 179)
         NoEspecifica.Name = "NoEspecifica"
-        NoEspecifica.Size = New Size(120, 24)
+        NoEspecifica.Size = New Size(114, 22)
         NoEspecifica.TabIndex = 8
         NoEspecifica.TabStop = True
         NoEspecifica.Text = "No Especifica"
@@ -126,96 +132,107 @@ Partial Class Form1
         ' 
         ' cbComuna
         ' 
+        cbComuna.BackColor = SystemColors.InactiveCaption
+        cbComuna.ForeColor = SystemColors.InfoText
         cbComuna.FormattingEnabled = True
-        cbComuna.Location = New Point(35, 260)
+        cbComuna.Location = New Point(35, 234)
         cbComuna.Name = "cbComuna"
-        cbComuna.Size = New Size(151, 28)
+        cbComuna.Size = New Size(151, 26)
         cbComuna.TabIndex = 9
         cbComuna.Text = "Comuna"
         ' 
         ' btnGuardar
         ' 
-        btnGuardar.Location = New Point(355, 31)
+        btnGuardar.BackColor = SystemColors.ActiveCaption
+        btnGuardar.Location = New Point(387, 24)
         btnGuardar.Name = "btnGuardar"
-        btnGuardar.Size = New Size(94, 29)
+        btnGuardar.Size = New Size(94, 26)
         btnGuardar.TabIndex = 10
         btnGuardar.Text = "Guardar"
-        btnGuardar.UseVisualStyleBackColor = True
+        btnGuardar.UseVisualStyleBackColor = False
         ' 
         ' txtCiudad
         ' 
-        txtCiudad.Location = New Point(197, 335)
+        txtCiudad.BackColor = SystemColors.InactiveCaption
+        txtCiudad.ForeColor = SystemColors.HotTrack
+        txtCiudad.Location = New Point(197, 302)
         txtCiudad.Name = "txtCiudad"
-        txtCiudad.Size = New Size(125, 27)
+        txtCiudad.Size = New Size(125, 26)
         txtCiudad.TabIndex = 11
         ' 
         ' txtObservacion
         ' 
-        txtObservacion.Location = New Point(197, 387)
+        txtObservacion.BackColor = SystemColors.InactiveCaption
+        txtObservacion.ForeColor = SystemColors.MenuHighlight
+        txtObservacion.Location = New Point(197, 348)
         txtObservacion.Name = "txtObservacion"
-        txtObservacion.Size = New Size(125, 27)
+        txtObservacion.Size = New Size(125, 26)
         txtObservacion.TabIndex = 12
         ' 
         ' Label4
         ' 
         Label4.AutoSize = True
-        Label4.Location = New Point(49, 335)
+        Label4.Location = New Point(49, 302)
         Label4.Name = "Label4"
-        Label4.Size = New Size(56, 20)
+        Label4.Size = New Size(51, 18)
         Label4.TabIndex = 13
         Label4.Text = "Ciudad"
         ' 
         ' Label5
         ' 
         Label5.AutoSize = True
-        Label5.Location = New Point(49, 389)
+        Label5.Location = New Point(49, 350)
         Label5.Name = "Label5"
-        Label5.Size = New Size(91, 20)
+        Label5.Size = New Size(88, 18)
         Label5.TabIndex = 14
         Label5.Text = "Observacion"
         ' 
         ' btnBuscar
         ' 
-        btnBuscar.Location = New Point(363, 389)
+        btnBuscar.BackColor = SystemColors.ActiveCaption
+        btnBuscar.Location = New Point(387, 140)
         btnBuscar.Margin = New Padding(3, 4, 3, 4)
         btnBuscar.Name = "btnBuscar"
-        btnBuscar.Size = New Size(86, 31)
+        btnBuscar.Size = New Size(94, 28)
         btnBuscar.TabIndex = 15
         btnBuscar.Text = "Buscar"
-        btnBuscar.UseVisualStyleBackColor = True
+        btnBuscar.UseVisualStyleBackColor = False
         ' 
         ' Label6
         ' 
         Label6.AutoSize = True
         Label6.Location = New Point(102, 1)
         Label6.Name = "Label6"
-        Label6.Size = New Size(96, 20)
+        Label6.Size = New Size(97, 18)
         Label6.TabIndex = 16
         Label6.Text = "Ej.136700367"
         ' 
         ' btnEliminar
         ' 
-        btnEliminar.Location = New Point(357, 87)
+        btnEliminar.BackColor = SystemColors.ActiveCaption
+        btnEliminar.Location = New Point(387, 68)
         btnEliminar.Name = "btnEliminar"
-        btnEliminar.Size = New Size(94, 29)
+        btnEliminar.Size = New Size(94, 26)
         btnEliminar.TabIndex = 17
         btnEliminar.Text = "Eliminar"
-        btnEliminar.UseVisualStyleBackColor = True
+        btnEliminar.UseVisualStyleBackColor = False
         ' 
         ' btnModificar
         ' 
-        btnModificar.Location = New Point(355, 141)
+        btnModificar.BackColor = SystemColors.ActiveCaption
+        btnModificar.Location = New Point(387, 107)
         btnModificar.Name = "btnModificar"
-        btnModificar.Size = New Size(94, 29)
+        btnModificar.Size = New Size(94, 26)
         btnModificar.TabIndex = 18
         btnModificar.Text = "Modificar"
-        btnModificar.UseVisualStyleBackColor = True
+        btnModificar.UseVisualStyleBackColor = False
         ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(8F, 18F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(493, 451)
+        BackColor = SystemColors.ActiveCaption
+        ClientSize = New Size(493, 406)
         Controls.Add(btnModificar)
         Controls.Add(btnEliminar)
         Controls.Add(Label6)
@@ -235,6 +252,9 @@ Partial Class Form1
         Controls.Add(Label3)
         Controls.Add(Label2)
         Controls.Add(Label1)
+        Font = New Font("Tahoma", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ForeColor = SystemColors.ActiveCaptionText
+        FormBorderStyle = FormBorderStyle.Fixed3D
         Name = "Form1"
         Text = "Eva2"
         ResumeLayout(False)
